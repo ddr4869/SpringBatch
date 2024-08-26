6dde848895cccae7d2239cda55e172f63c979fdb
ddr4869 <bikdu@naver.com>
2024-08-26 14:33:36 +0900

    7. Controller & Scheduller 적용

---
+++ src/main/java/batch/springbatch/controller/MainController.java
@@ -0,0 +1,30 @@
+package batch.springbatch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    @GetMapping("/first")
+    public String firstApi(@RequestParam("value") String value) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", value)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+        return "ok";
+    }
+
+}
---
+++ src/main/java/batch/springbatch/schedule/FirstScheduler.java
@@ -0,0 +1,34 @@
+package batch.springbatch.schedule;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+@RequiredArgsConstructor
+public class FirstScheduler {
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    @Scheduled(cron = "10 * * * * *", zone = "Asia/Seoul")
+    public void runFirstJob() throws Exception {
+
+        System.out.println("first schedule start");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+        String date = dateFormat.format(new Date());
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", date)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+    }
+}
