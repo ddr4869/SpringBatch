8496ae669c24039f3a6d56588be5236769945913
ddr4869 <bikdu@naver.com>
2024-08-26 13:09:53 +0900

    first commit

---
+++ src/main/java/batch/spring_batch/TomApplication.java
@@ -0,0 +1,13 @@
+package batch.spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batch/spring_batch/TomApplicationTests.java
@@ -0,0 +1,13 @@
+package batch.spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
